7503a40d4ff95b8b93a53c8fbc52b2f19410766a
TheQahnaarin <61778245+TheQahnaarin@users.noreply.github.com>
2020-03-17 13:03:10 +0100

    Hello

---
+++ Unity/SKP/Assets/Player_Movment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movment : MonoBehaviour
+{
+
+
+    public float Speed = 5;
+
+  
+
+
+    public void FixedUpdate()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime * Speed);
+    }
+}
